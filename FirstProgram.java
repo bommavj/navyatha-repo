d1b5c07652e3651c4c950123ff9c8d0fc4fda789
bommavj <vijay.bomma@gmail.com>
2021-10-21 13:43:06 -0400

    java changes second prgorssa rfht

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,7 +1,7 @@
 public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("First Program");
-        System.out.println("Navytha");
+        System.out.println("Navyatha");
         System.out.println("Vijay");
     }
 }
