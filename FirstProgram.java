ac2c4fffcf9d8a52c3ff98cedb6d9893de7b4000
bommavj <vijay.bomma@gmail.com>
2021-10-21 13:35:16 -0400

    java changes second prgorssa

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,6 +1,6 @@
 public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("First Program");
-        System.out.println("Second Progra");
+        System.out.println("Navytha");
     }
 }
